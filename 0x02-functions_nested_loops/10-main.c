6f53e275ae9ff2010779f5332ce8b09da518095b
AbayomiTech <rolandjeffrey112@gmail.com>
2023-03-22 06:04:11 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,14 @@
+#include "main-h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * 
+ * Return: sum of a and b
+ */
+int main(void)
+{
+int n;
+n = add(89, 9);
+printf("%\n", n);
+return (0);
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main_h"
+/**
+ * _islower - check if char is lowercase
+ * 0c: is the char to be checked 
+ * Return: ! if char is lowercase, otherwise 0.
+ */
+int _islower(int c)
+{
+if (c>= 'a' && c <= 'z')
+return (i);
+else 
+return (0)
+}
